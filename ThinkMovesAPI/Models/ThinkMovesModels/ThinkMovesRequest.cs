005be99701bc563ec5da5845bebf0c8972692789
AshayWindows <ashaykargaonkar@gmail.com>
2025-04-02 13:28:49 -0500

    Little changes

--- ThinkMovesAPI/Models/ThinkMovesModels/ThinkMovesRequest.cs
+++ ThinkMovesAPI/Models/ThinkMovesModels/ThinkMovesRequest.cs
@@ -1,7 +1,10 @@
-﻿namespace PortfolioAPI.Models.ThinkMovesAIModels
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PortfolioAPI.Models.ThinkMovesAIModels
 {
     public class ThinkMovesRequest
     {
+        [FromForm]
         public List<IFormFile> ScanImages { get; set;}
     }
 }
