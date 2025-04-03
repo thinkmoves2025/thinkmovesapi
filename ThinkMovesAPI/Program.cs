b5ebe39acf8eb4979d1d605f0815ce81f30228ea
AshayWindows <ashaykargaonkar@gmail.com>
2025-04-03 13:06:46 -0500

    Change basic

--- ThinkMovesAPI/Program.cs
+++ ThinkMovesAPI/Program.cs
@@ -29,7 +29,7 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins()
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
